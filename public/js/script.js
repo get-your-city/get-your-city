3685ccb2ecf5a0cdbdd001df0541e2c8dfa50d35
Michael Oberst <mioberst@gmx.de>
2022-07-15 12:01:38 +0200

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("get-your-city JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/get-your-city";
+
+module.exports = MONGO_URI;
