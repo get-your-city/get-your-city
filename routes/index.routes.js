81748a9a6be6e4c0174cffbd26276325ebcfecae
Ivona Pepic <ivona.pepic@gmail.com>
2022-07-15 17:43:39 +0200

    city routes done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,45 @@
 const router = require("express").Router();
+const City = require("../models/City.model");
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+
+// READ: List of all citys
+router.get("/citys", (req, res, next) => {
+    City.find()
+      .then( citysArr => {
+        res.render("auth/citys", {citysArr});
+        })
+        
+      .catch( (error) => {
+        console.log("Error getting data from DB", error);
+        next(error);
+      })
+    })
+
+
+    // CREATE: Render form
+router.get("/auth/create", (req, res, next) => {
+      res.render("auth/citys-create");
+    })
+
+// CREATE: Process form
+router.post("/auth/create", (req, res, next) => {
+
+  const cityDetails = {
+    name: req.body.name,
+    country: req.body.country,
+    description: req.body.description,
+    // image: req.body.image,
+  };
+
+  City.create(cityDetails)
+    .then( () => {
+      res.redirect("/citys");
+    })
+    .catch( (error) => {
+      console.log("Error adding city in the DB", error);
+      next(error);
+    })
+})
 
 module.exports = router;
