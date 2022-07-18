292ab67c1cbca2e325cd0356794068f5c60e5c78
Michael Oberst <mioberst@gmx.de>
2022-07-18 09:38:03 +0200

    fix bug in logout form

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -139,7 +139,7 @@ router.post("/login", isLoggedOut, (req, res, next) => {
     });
 });
 
-router.get("/logout", isLoggedIn, (req, res) => {
+router.post("/logout", isLoggedIn, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       return res
